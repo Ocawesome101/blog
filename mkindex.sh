655f86888ece673d22a0a4e866a9704f03232b41
Ocawesome101 <ocawesome101@gmail.com>
2020-08-08 23:00:07 -0400

    thing

--- mkindex.sh
+++ mkindex.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Simple script to generate an index.html
 
-site="https://oz-craft.pickardayune.com/blog"
+site="https://oz-craft.pickardayune.com/blog/pages"
 
 echo "<html><title>Ocawesome101's Blog</title><body style="background-color:\#000\;color:\#fff\;font-family:Courier">" > index.html
 cat base.txt >> index.html
 echo "<br><br>" >> index.html
-for file in $(ls blog | sort); do
-  header=$(lua getheader.lua "blog/$file")
+for file in $(ls pages | sort); do
+  header=$(lua getheader.lua "pages/$file")
   echo "<a href="$site/$file">$header</a><br>" >> index.html
 done
 echo '</body></html>' >> index.html

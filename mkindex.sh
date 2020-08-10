8181b4acb9af7b593734e76e473a513536aa9f08
Ocawesome101 <ocawesome101@gmail.com>
2020-08-09 22:39:38 -0400

    fix mkindex

--- mkindex.sh
+++ mkindex.sh
@@ -7,7 +7,7 @@ echo "<!DOCTYPE html> <link rel=\"stylesheet\" href=\"https://oz-craft.pickarday
 cat base.txt >> index.html
 echo "<br><br>" >> index.html
 for file in $(ls pages | sort); do
-  header=$(lua getheader.lua \"pages/$file\")
+  header=$(lua getheader.lua "pages/$file")
   echo "<a href=\"$site/$file\">$header</a><br>" >> index.html
 done
 echo '</body></html>' >> index.html

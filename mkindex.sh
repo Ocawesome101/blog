0936b331c87d5d750048d3cee20c637069a1dd0f
Ocawesome101 <ocawesome101@gmail.com>
2020-08-10 02:34:26 +0000

    a post idk

--- mkindex.sh
+++ mkindex.sh
@@ -3,11 +3,11 @@
 
 site="https://oz-craft.pickardayune.com/blog/pages"
 
-echo "<html><title>Ocawesome101's Blog</title><body style="background-color:\#000\;color:\#fff\;font-family:Courier">" > index.html
+echo "<!DOCTYPE html> <link rel=\"stylesheet\" href=\"https://oz-craft.pickardayune.com/blog/style.css\"> <html><title>Ocawesome101's Blog</title><body>" > index.html
 cat base.txt >> index.html
 echo "<br><br>" >> index.html
 for file in $(ls pages | sort); do
-  header=$(lua getheader.lua "pages/$file")
-  echo "<a href="$site/$file">$header</a><br>" >> index.html
+  header=$(lua getheader.lua \"pages/$file\")
+  echo "<a href=\"$site/$file\">$header</a><br>" >> index.html
 done
 echo '</body></html>' >> index.html
